e55b7be34d6b90587efe18f3bad1af9afe24761c
klotzy13 <x.arslan2006@gmail.com>
2024-06-06 09:52:38 +0500

    Init

---
+++ WebApplication1/Controllers/HotelCommentsController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication1.Entities;
+
+namespace WebApplication1.Controllers
+{
+    public class HotelCommentsController : ApiController
+    {
+        private ToursEntities db = new ToursEntities();
+
+        // GET: api/HotelComments
+        public IQueryable<HotelComment> GetHotelComment()
+        {
+            return db.HotelComment;
+        }
+        [Route("api/getHotelComments")]
+        public IHttpActionResult GetHotelCommets(int hotelid)
+        {
+            var hotelCommets = db.HotelComment.ToList().Where(p => p.Hotelid == hotelid).ToList();
+            return Ok(hotelCommets);
+        }
+
+
+
+        // GET: api/HotelComments/5
+        [ResponseType(typeof(HotelComment))]
+        public IHttpActionResult GetHotelComment(int id)
+        {
+            HotelComment hotelComment = db.HotelComment.Find(id);
+            if (hotelComment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(hotelComment);
+        }
+
+        // PUT: api/HotelComments/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutHotelComment(int id, HotelComment hotelComment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != hotelComment.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(hotelComment).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HotelCommentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/HotelComments
+        [ResponseType(typeof(HotelComment))]
+        public IHttpActionResult PostHotelComment(HotelComment hotelComment)
+        {
+            hotelComment.CreationDate = DateTime.Now;
+
+            if (string.IsNullOrWhiteSpace(hotelComment.Author) || hotelComment.Author.Length > 100)
+                ModelState.AddModelError("Author", "Athor is required string up to 100 symbols.");
+            if (string.IsNullOrWhiteSpace(hotelComment.Text))
+                ModelState.AddModelError("Text", "Text is required string.");
+            if (!(db.Hotel.ToList().FirstOrDefault(p => p.id == hotelComment.Hotelid) is Hotel))
+                ModelState.AddModelError("HotelId", "HotelId is hotel's id from datdbase.");
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.HotelComment.Add(hotelComment);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (HotelCommentExists(hotelComment.id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = hotelComment.id }, hotelComment);
+        }
+
+        // DELETE: api/HotelComments/5
+        [ResponseType(typeof(HotelComment))]
+        public IHttpActionResult DeleteHotelComment(int id)
+        {
+            HotelComment hotelComment = db.HotelComment.Find(id);
+            if (hotelComment == null)
+            {
+                return NotFound();
+            }
+
+            db.HotelComment.Remove(hotelComment);
+            db.SaveChanges();
+
+            return Ok(hotelComment);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool HotelCommentExists(int id)
+        {
+            return db.HotelComment.Count(e => e.id == id) > 0;
+        }
+    }
+}
---
+++ WebApplication1/Models/ResponseHotel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Entities;
+
+namespace WebApplication1.Models
+{
+    public class ResponseHotel
+    {
+        public ResponseHotel(Hotel hotel) 
+        {
+            Id= hotel.id;
+            Name= hotel.Name;
+            CountOfStars= hotel.CountOfStars;
+            CountryName = hotel.Country.Name;
+            HotelImage = hotel.Hotelimage.ToList().FirstOrDefault()?.ImageSource;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CountOfStars { get; set; }
+        public string CountryName { get; set; }
+        public byte[] HotelImage { get; set; }
+
+    }
+}
